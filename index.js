f5ab8da7450b9431f9b65796c444c261a0072cdb
1KitCat1 <denischool119@gmail.com>
2023-06-10 21:42:06 +0300

    Basic chat

--- index.js
+++ index.js
@@ -2,3 +2,27 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const auth = require('./auth');
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/static/index.html');
+  });
+  
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+
+    socket.on('chat message', (msg) => {
+        console.log('Received message:', msg);
+        io.emit('chat message', msg);
+    });
+});
+
+const port = 3000;
+http.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
